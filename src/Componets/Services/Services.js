181e375f8eaad30e453352741b088545bf7f9b72
krchavan <krchavan1999@gmail.com>
2024-03-02 17:54:07 +0530

    service page work

--- src/Componets/Services/Services.js
+++ src/Componets/Services/Services.js
@@ -1,19 +1,130 @@
 import React from 'react'
-import Govt from './Govt'
-import Softskill from './Softskill'
-import Isocer from './Isocer'
-import Isocon from './Isocon'
+import './../Services/Services.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function Services() {
   return (
+    
     <div>
-      Services
-      <Govt />
-      <Isocer />
-      <Softskill />
-      <Isocon />
-      
+			<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet"/>
+
+<section class="section services-section" id="services">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="section-title">
+                    <h2>What I Do</h2>
+                    <p>Our Company Provides Different Training Programs For Employees Which Help The Organization To Grow Efficiently. As A Premier ISO Consultant And Training Company, We Aim At Delivering The Highest Quality Of ISO Consultant And Training Services To Businesses Across All Industries.Staff Motivational Training And Gold Testing & Valuation Training Programme . </p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            
+            <div class="col-sm-6 col-lg-4">
+                <div class="feature-box-1">
+                    <div class="icon">
+                        <i class="fa fa-desktop"></i>
+                    </div>
+                    <div class="feature-content">
+                        <h5>SOFT SKILL DEVELOPMENT PROGRAMME</h5>
+                         
+<li>Office Manners</li>
+<li>Staff Awareness</li>
+<li>Team Work and Team Building</li>
+<li>Time Management</li>
+<li>Positive Thinking</li>
+<li>Goal Setting</li>
+<li>Staff Motivation</li>
+<li>Personal & Personality Development</li>
+<li>Effectives Communication Skill</li>
+<li>Sale and Marketing Management</li>
+<li>Secrete of Life</li>
+<li>Money Management/ Thoughts about Money</li>
+<li>Law of Success / Art of Success</li>
+<li>Waste Management </li>
+<li>Stress Management</li>
+<li>Chanakya Niti</li>
+<li>Customer Relationship Management</li>
+<li>Telephonic Manners / Etiquettes</li>
+<li>Strategic Management</li>
+<li>Leadership Skills</li>
+<li>Productivity Management</li>
+
+                    </div>
+                </div>
+            </div>
+            
+            
+            <div class="col-sm-6 col-lg-4">
+                <div class="feature-box-1">
+                    <div class="icon">
+                        <i class="fa fa-user"></i>
+                    </div>
+                    <div class="feature-content">
+                        <h5>Different Layout</h5>
+                        <p>I design and develop services for customers of all sizes, specializing in creating stylish, modern websites.</p>
+                    </div>
+                </div>
+            </div>
+            
+            
+            <div class="col-sm-6 col-lg-4">
+                <div class="feature-box-1">
+                    <div class="icon">
+                        <i class="fa fa-comment"></i>
+                    </div>
+                    <div class="feature-content">
+                        <h5>Make it Simple</h5>
+                        <p>I design and develop services for customers of all sizes, specializing in creating stylish, modern websites.</p>
+                    </div>
+                </div>
+            </div>
+            
+            
+            <div class="col-sm-6 col-lg-4">
+                <div class="feature-box-1">
+                    <div class="icon">
+                        <i class="fa fa-image"></i>
+                    </div>
+                    <div class="feature-content">
+                        <h5>Responsiveness</h5>
+                        <p>I design and develop services for customers of all sizes, specializing in creating stylish, modern websites.</p>
+                    </div>
+                </div>
+            </div>
+            
+            
+            <div class="col-sm-6 col-lg-4">
+                <div class="feature-box-1">
+                    <div class="icon">
+                        <i class="fa fa-th"></i>
+                    </div>
+                    <div class="feature-content">
+                        <h5>Testing for Perfection</h5>
+                        <p>I design and develop services for customers of all sizes, specializing in creating stylish, modern websites.</p>
+                    </div>
+                </div>
+            </div>
+            
+            
+            <div class="col-sm-6 col-lg-4">
+                <div class="feature-box-1">
+                    <div class="icon">
+                        <i class="fa fa-cog"></i>
+                    </div>
+                    <div class="feature-content">
+                        <h5>Advanced Options</h5>
+                        <p>I design and develop services for customers of all sizes, specializing in creating stylish, modern websites.</p>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
     </div>
+</section>
+		</div>
+    
   )
 }
 
